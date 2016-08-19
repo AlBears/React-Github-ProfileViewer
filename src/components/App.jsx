b3e3d33ca96a358947216fcf565999bd361676b6
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-08-19 09:07:35 +0300

    Search component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './github/Profile.jsx';
+import Search from './github/Search.jsx';
 
 class App extends Component {
 	constructor(props){
@@ -43,6 +44,14 @@ class App extends Component {
 			}.bind(this)
 		});
 	}
+
+	handleFormSubmit(username){
+		this.setState({username: username}, function(){
+			this.getUserData();
+			this.getUserRepos();
+		});
+	}
+
 	componentDidMount(){
 		this.getUserData();
 		this.getUserRepos();
@@ -50,6 +59,7 @@ class App extends Component {
 	render(){
 		return (
 				<div>
+					<Search onFormSubmit = {this.handleFormSubmit.bind(this)}/>
 					<Profile {...this.state}/>
 				</div>
 			)
